008e6d6c82a460b1ef7ea9fd90018865a56dab01
japhethca <chidiebere.anyigor@gmail.com>
2020-07-11 17:02:32 +0100

    add gallery test

--- src/components/__tests__/ProviderGallery.test.js
+++ src/components/__tests__/ProviderGallery.test.js
@@ -2,8 +2,22 @@
 
 @jest-environment jsdom
 */
-describe('Gallery', () => {
-  test('My Test Case', () => {
-    expect(true).toEqual(false)
-  })
-})
+import React from "react";
+import { render } from "@testing-library/react";
+import Gallery from "../ProviderGallery";
+
+describe("Gallery", () => {
+  test("should display providers", async () => {
+    const items = [
+      {
+        id: 1,
+        imageUrl: "",
+        name: "test",
+        description: "testing providers",
+      },
+    ];
+    const { findByText } = render(<Gallery items={items} />);
+    const el = await findByText(items[0].description);
+    expect(el).toBeInTheDocument();
+  });
+});
